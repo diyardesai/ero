8c7d39eeda35536139561c9976cd9bdd7b0d0e01
diyardesai <diyardesai@gmail.com>
2025-02-14 15:30:50 -0800

    added constant marker at LA

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
+// Initialize and add the map
 let map;
 let geocoder;
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: -25.344, lng: 131.031 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  geocoder = new google.maps.Geocoder();
+  // The map, centered at Uluru
 
-function initMap() {
-  //  default location 
-    map = new google.maps.Map(document.getElementById("map-canvas"), {
-        center: { lat: 34.0522, lng: -118.2437 }, // Los Angeles
-        zoom: 12,
-    });
+  map = new Map (document.getElementById("map"), {
+    zoom: 12,
+    center: { lat: 34.0522, lng: -118.2437 }, // Los Angeles,
+    mapId: "DEMO_MAP_ID",
+  });
 
-    // geocoder
-    geocoder = new google.maps.Geocoder();
-}
-
-function findRoute() {
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: { lat: 34.0522, lng: -118.2437 }, // Los Angeles,
+    title: "Uluru",
+  });
+  function findRoute() {
     let address = document.getElementById("destination").value;
 
     // If address is provided, geocode it
@@ -22,4 +33,35 @@ function findRoute() {
         alert("Please enter a destination!");
     }
 }
+}
 
+initMap();
+
+
+
+// let map;
+// Initialize and add the map
+//const { Map } = await google.maps.importLibrary("maps");
+//const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+//async function initMap() {
+  //  default location 
+    // const { Map } = await google.maps.importLibrary("maps");
+    // const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+//     map = new google.maps.Map(document.getElementById("map-canvas"), {
+//         center: { lat: 34.0522, lng: -118.2437 }, // Los Angeles
+//         zoom: 12,
+//     });
+
+
+//     // geocoder
+
+
+//     const marker = new AdvancedMarkerElement({
+//         map: map,
+//         position: { lat: 34.0522, lng: -118.2437 },
+//         title: "LA",
+//       });
+// }
+
+
+// initMap();
