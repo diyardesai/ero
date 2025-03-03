abfaea22ea1f544b06a9a945e22046bd69f1adf6
diyardesai <diyardesai@gmail.com>
2025-03-03 15:48:11 -0800

    added 3 buttons: driving, cycling, transit

--- script.js
+++ script.js
@@ -8,39 +8,14 @@ let directionsRenderer;
 function initMap() {
   directionsService = new google.maps.DirectionsService();
   directionsRenderer = new google.maps.DirectionsRenderer();
-
-  //Chicago
   var chicago = new google.maps.LatLng(41.850033, -87.6500523);
   var mapOptions = {
       zoom: 7,
       center: chicago
   };
-
   map = new google.maps.Map(document.getElementById("map"), mapOptions);
   directionsRenderer.setMap(map);
 }
-/*
-function findRoute() {
-    let address = document.getElementById("destination").value;
-
-    // If address is provided, geocode it
-    if (address) {
-        geocodeAddress(address);
-    } else {
-        alert("Please enter a destination!");
-    }
-}
-function geocodeAddress(address) {
-  geocoder.geocode({ address: address }, function (results, status) {
-      if (status === "OK") {
-          let destination = results[0].geometry.location;
-          let start = { lat: 34.0522, lng: -118.2437 }; // user
-          calculateRoute(start, destination);
-      } else {
-          alert("not successful for the following reason: " + status);
-      }
-  });
-}*/
 function calcRoute() {
   var start = document.getElementById("start").value;
     var end = document.getElementById("end").value;
@@ -53,7 +28,7 @@ function calcRoute() {
     var request = {
         origin: start,
         destination: end,
-        travelMode: google.maps.TravelMode.DRIVING // Change to WALKING, BICYCLING, or TRANSIT if needed
+        travelMode: google.maps.TravelMode.DRIVING 
     };
 
     directionsService.route(request, function (result, status) {
