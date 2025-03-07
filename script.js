08e7d9ffa137be187289eff005424ed12afb51f9
diyardesai <diyardesai@gmail.com>
2025-03-07 15:27:11 -0800

    added arrival and departure time buttons - not working yet

--- script.js
+++ script.js
@@ -24,8 +24,10 @@ function setTravelMode(mode) {
 
 
 function calcRoute() {
-  var start = document.getElementById("start").value;
+    var start = document.getElementById("start").value;
     var end = document.getElementById("end").value;
+    var timeInput = document.getElementById("time").value;
+    var timeType = document.getElementById("timeType").value;
 
     if (!start || !end) {
         alert("Please enter both a start and destination address.");
@@ -37,16 +39,24 @@ function calcRoute() {
         destination: end,
         travelMode: google.maps.TravelMode[selectedMode]
     };
+    if (timeInput){
+        var selectedTime = new Date(timeInput);
 
+        if(timeType == "departure"){
+            request.departureTime = selectedTime;
+        } else if(timeType == "arrival"){
+            request.arrivalTime = selectedTime;
+        }
+    }
     directionsService.route(request, function (result, status) {
         if (status === google.maps.DirectionsStatus.OK) {
             directionsRenderer.setDirections(result);
 
             var leg = result.routes[0].legs[0]; 
             document.getElementById("output").innerHTML = `
-                <strong>Estimated Travel Time:</strong> ${leg.duration.text} <br>
-                <strong>Distance:</strong> ${leg.distance.text}
-            `;
+            <strong>Estimated Travel Time:</strong> ${leg.duration.text} <br>
+            <strong>Distance:</strong> ${leg.distance.text}
+        `;
         } else {
             alert("Could not calculate route: " + status);
         }
